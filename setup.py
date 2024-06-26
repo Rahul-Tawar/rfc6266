96ac53b58fa302a1d61f9afcd58e4df341e7c6e0
rahultawar <140714292+Rahul-Tawar@users.noreply.github.com>
2024-06-26 14:34:50 +0530

    Update setup.py
    Remove use_2to3
    Update classifier from tuple to list

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ setup(
     platforms='OS-independent',
     py_modules=['rfc6266', 'test_rfc6266'],
     install_requires=['LEPL'],
-    use_2to3=True,
     long_description=open('README').read(),
-    classifiers=(
+    classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython',
@@ -23,6 +22,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Topic :: Internet :: WWW/HTTP',
-    ),
+    ],
 )
 
